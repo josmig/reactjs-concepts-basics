3b80bc718e4934f805703f5fde3cc90da9ec6d7e
josmig <pantigosomiguel59@gmail.com>
2021-09-10 13:18:38 -0500

    destructuring - valores por defecto y funciones por props

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Saludar from './components/Saludar';
 function App() {
 
   const user = {
-    nombre:'Miguel Pantigoso',
+    /* nombre:'Miguel Pantigoso', */
     edad:23,
     color:'Azul'
   }
-
+  //pasando funciones entre componentes
+  const saludo= (nombre ="pepe") => {
+    console.log(`Hola ${nombre}`);
+  }
   return (
     <div className="App">
       <h1>Aprendiendo REACT</h1>
       {/* <Saludar name="Jose Miguel Pantigoso puraca" edad="23"/> */}
-      <Saludar userInfo={user}/>
+      <Saludar userInfo={user} saludar={saludo}/>
       
     </div>    
   );
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 export default function Saludar( props ){
 
-    console.log(props);
-    console.log(props.userInfo);
+    /* console.log(props); */
+    /* console.log(props.userInfo); */
 
+    const {userInfo,saludar} = props;
+    const{nombre} = userInfo;
+    /* console.log(userInfo); */
     return(
         <div>
             {/* <h2>Hola, {props.name} tiene {props.edad} años</h2> */}
-            <h2>Hola , {props.userInfo.nombre} tiene {props.userInfo.edad}, y su color favorito es {props.userInfo.color}</h2>
+            {/* <h2>Hola , {props.userInfo.nombre} tiene {props.userInfo.edad}, y su color favorito es {props.userInfo.color}</h2> */}
+            {/* <button onClick={()=>props.saludar(props.userInfo.nombre)}>Enviar Saludo</button> */}
+            <button onClick={()=> saludar(nombre)}>Saludar</button>
         </div>
     );
 }
