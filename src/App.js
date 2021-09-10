672310c903ffa0c8b0ff0dbda224a00bd8b22677
josmig <pantigosomiguel59@gmail.com>
2021-09-10 17:57:48 -0500

    uso de los useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Saludar from './components/Saludar';
 
@@ -17,14 +17,20 @@ function App() {
   }
 
   //Utilizando Hooks
-  const[estadoCarro, setStateCar] = useState(false);
+  const[estadoCarro, setStateCar] = useState(false); //useState
+  const[contador, setContador] = useState(0); // useState con useEffect
+
+  useEffect(() =>{
+    console.log('Total : ' + contador);
+  },[contador])
 
   const  estado = () => {
     /* console.log("Encender / Apagar"); */
     //Con esto cogemos el valor 
     /* setStateCar(!estadoCarro); */
     //con esto cogemos el valor del estado actual
-    setStateCar(prevValue => !prevValue )
+    setStateCar(prevValue => !prevValue );
+    setContador(contador + 1 );
   }
   return (
     <div className="App">
@@ -32,6 +38,7 @@ function App() {
       {/* <Saludar name="Jose Miguel Pantigoso puraca" edad="23"/> */}
       <Saludar userInfo={user} saludar={saludo}/>
       <h3>El coche esta : {estadoCarro ? "Encendido" : "Apagado"}</h3>
+      <h5>Ejecuciones : {contador}</h5>
       <button onClick={estado}>Encender/Apagar</button>
     </div>    
   );
