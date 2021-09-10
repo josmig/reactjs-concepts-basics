4936fef04b2bc430736a618f207714f9929d07f7
josmig <pantigosomiguel59@gmail.com>
2021-09-10 17:40:56 -0500

    introducción a los hooks proando useStatus para la modificacion de estados

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Saludar from './components/Saludar';
 
@@ -10,15 +11,28 @@ function App() {
     color:'Azul'
   }
   //pasando funciones entre componentes
-  const saludo= (nombre ="pepe") => {
-    console.log(`Hola ${nombre}`);
+  const saludo= (nombre ="pepe", edad=10) => {
+    //templates literales
+    console.log(`Hola ${nombre} tengo ${edad} años`);
+  }
+
+  //Utilizando Hooks
+  const[estadoCarro, setStateCar] = useState(false);
+
+  const  estado = () => {
+    /* console.log("Encender / Apagar"); */
+    //Con esto cogemos el valor 
+    /* setStateCar(!estadoCarro); */
+    //con esto cogemos el valor del estado actual
+    setStateCar(prevValue => !prevValue )
   }
   return (
     <div className="App">
       <h1>Aprendiendo REACT</h1>
       {/* <Saludar name="Jose Miguel Pantigoso puraca" edad="23"/> */}
       <Saludar userInfo={user} saludar={saludo}/>
-      
+      <h3>El coche esta : {estadoCarro ? "Encendido" : "Apagado"}</h3>
+      <button onClick={estado}>Encender/Apagar</button>
     </div>    
   );
 }
