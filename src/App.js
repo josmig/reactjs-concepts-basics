c597699bb15d98583131efceb2e1288751ecd5fb
josmig <pantigosomiguel59@gmail.com>
2021-09-10 12:46:00 -0500

    pasando objetos por los props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import Saludar from './components/Saludar';
 
 
 function App() {
+
+  const user = {
+    nombre:'Miguel Pantigoso',
+    edad:23,
+    color:'Azul'
+  }
+
   return (
     <div className="App">
       <h1>Aprendiendo REACT</h1>
-      <Saludar name="Jose Miguel Pantigoso puraca" edad="23"/>
-      <Saludar name="Jose Miguel Pantigoso puraca"/>
+      {/* <Saludar name="Jose Miguel Pantigoso puraca" edad="23"/> */}
+      <Saludar userInfo={user}/>
       
     </div>    
   );
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -3,9 +3,12 @@ import React from 'react';
 export default function Saludar( props ){
 
     console.log(props);
+    console.log(props.userInfo);
+
     return(
         <div>
-            <h2>Hola, {props.name} tiene {props.edad} años</h2>
+            {/* <h2>Hola, {props.name} tiene {props.edad} años</h2> */}
+            <h2>Hola , {props.userInfo.nombre} tiene {props.userInfo.edad}, y su color favorito es {props.userInfo.color}</h2>
         </div>
     );
 }
